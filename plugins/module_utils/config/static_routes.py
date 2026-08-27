0ddd7b7d35bddae8961f77ddd1b2a0ab862bc7be
Lachlan Archibald <lachlan.archibald@opengear.com>
2026-08-27 12:16:03 +1000

    feat(static_routes): Enhance module to meet collection release standard (#85)
    The following improvements have been made to the static_routes module to
    meet the desired standard for release:
    
    - Add all fields from current API spec
    - Add check mode simulation and diff mode output
    - Expand unit tests to cover all states, diff, and check mode
    - Update module DOCUMENTATION and EXAMPLES with full field descriptions
    - Add example playbook

--- plugins/module_utils/config/static_routes.py
+++ plugins/module_utils/config/static_routes.py
@@ -9,6 +9,7 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 
 from copy import deepcopy
+import json
 
 from ansible.module_utils.connection import ConnectionError
 
@@ -19,6 +20,7 @@ from ansible_collections.opengear.ng.plugins.module_utils.utils.utils import (
     dict_diff,
     dict_merge,
     find_instance_id,
+    is_subset,
     remove_empties,
     to_list,
 )
@@ -40,14 +42,15 @@ class StaticRoutes(ConfigBase):
 
     def __init__(self, module):
         super(StaticRoutes, self).__init__(module)
+        self.current_state = {}
 
-    def get_static_routes_facts(self):
+    def get_static_routes_facts(self, data=None):
         """ Get the 'facts' (the current configuration)
 
-        :rtype: A dictionary
-        :returns: The current configuration as a dictionary
+        :rtype: A list
+        :returns: The current configuration as a list
         """
-        facts, _warnings = Facts(self._module).get_facts(self.gather_subset, self.gather_network_resources)
+        facts, _warnings = Facts(self._module).get_facts(self.gather_subset, self.gather_network_resources, data)
         static_routes_facts = facts['ansible_network_resources'].get('static_routes')
         if not static_routes_facts:
             return []
@@ -68,33 +71,108 @@ class StaticRoutes(ConfigBase):
         else:
             existing_static_routes_facts = {}
         if self.state in self.ACTION_STATES or self.state == 'rendered':
-            commands.extend(self.set_config(existing_static_routes_facts))
+            commands.extend(self.set_config(existing_static_routes_facts, warnings))
         if commands and self.state in self.ACTION_STATES:
             if not self._module.check_mode:
                 for command in commands:
+                    route_id = None
+                    if command['method'] in ['PUT', 'DELETE'] and '/' in command['path'].rstrip('/'):
+                        parts = command['path'].rstrip('/').split('/')
+                        if parts[-1] != 'static_routes':
+                            route_id = parts[-1]
                     try:
-                        self._connection.send_request(command['data'], command['path'], command['method'])
+                        response = self._connection.send_request(command['data'], command['path'], command['method'])
+                        if command['method'] == 'DELETE':
+                            self.current_state.pop(route_id, None)
+                        elif route_id and command['method'] == 'PUT':
+                            self.current_state[route_id] = response['static_route']
+                        elif command['method'] == 'PUT' and command['path'].rstrip('/').endswith('static_routes'):
+                            # Bulk PUT (overridden)
+                            self.current_state = {r['id']: r for r in response.get('static_routes', [])}
+                        else:
+                            self.current_state[response['static_route']['id']] = response['static_route']
                     except ConnectionError as exc:
                         if not exc.args[0].startswith('Expecting value:'):
                             raise exc
+                        if route_id:
+                            self.current_state.pop(route_id, None)
+            else:
+                # Simulate state changes for check mode + diff
+                for command in commands:
+                    if command['method'] == 'DELETE':
+                        parts = command['path'].rstrip('/').split('/')
+                        route_id = parts[-1]
+                        self.current_state.pop(route_id, None)
+                    elif command['method'] == 'PUT':
+                        parts = command['path'].rstrip('/').split('/')
+                        if parts[-1] != 'static_routes':
+                            route_id = parts[-1]
+                            if route_id in self.current_state:
+                                self.current_state[route_id].update(command['data']['static_route'])
+                        else:
+                            # Bulk PUT (overridden)
+                            new_routes = command['data'].get('static_routes', [])
+                            self.current_state = {}
+                            for i, r in enumerate(new_routes):
+                                temp_key = 'check-{0}'.format(i)
+                                self.current_state[temp_key] = r
+                    elif command['method'] == 'POST':
+                        data = command['data']['static_route']
+                        temp_key = 'check-{0}'.format(data.get('destination_address', ''))
+                        self.current_state[temp_key] = data
             result['changed'] = True
-        if self.state in self.ACTION_STATES:
-            result['commands'] = commands
+
+        result['commands'] = commands
         if self.state in self.ACTION_STATES or self.state == 'gathered':
-            changed_static_routes_facts = self.get_static_routes_facts()
+            changed_static_routes_facts = self.get_static_routes_facts(self.current_state.values())
         elif self.state == 'rendered':
             result['rendered'] = commands
         if self.state in self.ACTION_STATES:
             result['before'] = existing_static_routes_facts
             if result['changed']:
                 result['after'] = changed_static_routes_facts
+                if self._module._diff:
+                    diff_before = []
+                    diff_after = []
+
+                    existing_by_id = {r['id']: r for r in existing_static_routes_facts}
+
+                    for command in commands:
+                        if command['method'] == 'DELETE':
+                            route_id = command['path'].rstrip('/').split('/')[-1]
+                            if route_id in existing_by_id:
+                                diff_before.append(existing_by_id[route_id])
+                                diff_after.append({})
+
+                        elif command['method'] == 'PUT':
+                            parts = command['path'].rstrip('/').split('/')
+                            if parts[-1] != 'static_routes':
+                                route_id = parts[-1]
+                                if route_id in existing_by_id:
+                                    before = existing_by_id[route_id]
+                                    after = {**before, **command['data']['static_route']}
+                                    diff_before.append(before)
+                                    diff_after.append(after)
+                            else:
+                                # Bulk PUT (overridden)
+                                diff_before.extend(existing_static_routes_facts)
+                                diff_after.extend(command['data'].get('static_routes', []))
+
+                        elif command['method'] == 'POST':
+                            diff_before.append({})
+                            diff_after.append(command['data']['static_route'])
+
+                    result['diff'] = {
+                        'before': json.dumps(diff_before, indent=4) + '\n',
+                        'after': json.dumps(diff_after, indent=4) + '\n',
+                    }
         elif self.state == 'gathered':
             result['gathered'] = changed_static_routes_facts
 
         result['warnings'] = warnings
         return result
 
-    def set_config(self, existing_static_routes_facts):
+    def set_config(self, existing_static_routes_facts, warnings):
         """ Collect the configuration from the args passed to the module,
             collect the current configuration (as a dict from facts)
 
@@ -104,10 +182,10 @@ class StaticRoutes(ConfigBase):
         """
         want = self._module.params['config']
         have = existing_static_routes_facts
-        resp = self.set_state(want, have)
+        resp = self.set_state(want, have, warnings)
         return to_list(resp)
 
-    def set_state(self, want, have):
+    def set_state(self, want, have, warnings):
         """ Select the appropriate function based on the state provided
 
         :param want: the desired configuration as a dictionary
@@ -122,6 +200,8 @@ class StaticRoutes(ConfigBase):
             destination_id_map[route['destination_address']] = route['id']
             id_route_map[route['id']] = route
 
+        self.current_state = deepcopy(id_route_map)
+
         state = self._module.params['state']
         if state == 'overridden':
             commands = self._state_overridden(want, destination_id_map, id_route_map)
@@ -131,6 +211,8 @@ class StaticRoutes(ConfigBase):
             commands = self._state_merged(want, destination_id_map, id_route_map)
         elif state == 'replaced':
             commands = self._state_replaced(want, destination_id_map, id_route_map)
+        else:
+            commands = []
         return commands
 
     @staticmethod
@@ -144,12 +226,13 @@ class StaticRoutes(ConfigBase):
         commands = []
         for route in want:
             route_id = find_instance_id(destination_id_map, 'destination_address', route)
+            data = remove_empties(route)
             if route_id in id_route_map:
-                data = remove_empties(route)
                 data['id'] = route_id
-                if data == remove_empties(id_route_map[route_id]):
+                if is_subset(data, remove_empties(id_route_map[route_id])):
                     continue
-            command = command_builder({'static_route': route}, 'static_routes/', route_id)
+                data.pop('id', None)
+            command = command_builder({'static_route': data}, 'static_routes/', route_id)
             if command:
                 commands.append(command)
         return commands
@@ -175,7 +258,8 @@ class StaticRoutes(ConfigBase):
                 deleted_routes.pop(route_id)
 
         if len(deleted_routes) == len(id_route_map):
-            commands.append({'data': {'static_routes': want}, 'path': 'static_routes/', 'method': 'PUT'})
+            clean_want = [remove_empties(r) for r in want]
+            commands.append({'data': {'static_routes': clean_want}, 'path': 'static_routes/', 'method': 'PUT'})
         else:
             commands.extend(StaticRoutes._state_deleted(deleted_routes.values(), destination_id_map))
             commands.extend(StaticRoutes._state_replaced(want, destination_id_map, id_route_map))
